0b3aacc8a9fd86a9747016886413264dd580a418
Hassan Arjmandnia <hassanarjmandnia1998@gmail.com>
2024-01-28 06:09:51 +0330

    Add sample routes for note opreations.

--- Fast_API/note.py
+++ Fast_API/note.py
@@ -1,7 +1,6 @@
-from .schemas import UserTableCreate, UserTableLogin, UserTableChangePassword
+from .schemas import NoteTableAdd
 from .auth import oauth_2_schemes, AuthManager
 from fastapi import APIRouter, Depends
-from .user_modules import UserManager
 from .logger import loggers
 
 
@@ -9,5 +8,30 @@ note_router = APIRouter()
 
 
 @note_router.get("/test")
-async def test():
-    return await {"Hello world": "from note.py"}
+def test():
+    return {"Hello world": "from note.py"}
+
+
+@note_router.get("/show_notes")
+def show_notes():
+    return {"Hello world": "from note.py -> show notes"}
+
+
+@note_router.get("/show_note/{note_id}")
+def show_note(note_id: int):
+    return {"Hello world from note.py -> show note: note id:": note_id}
+
+
+@note_router.post("/add_note")
+def add_note(body_of_note: NoteTableAdd):
+    return {"Hello world from note.py -> Add note": body_of_note}
+
+
+@note_router.patch("/update_note/{note_id}")
+def update_note(note_id: int):
+    return {"Hello world from note.py -> Update note": note_id}
+
+
+@note_router.delete("/delete_note/{note_id}")
+def delete_note(note_id: int):
+    return {"Hello world from note.py -> Delete note": note_id}
